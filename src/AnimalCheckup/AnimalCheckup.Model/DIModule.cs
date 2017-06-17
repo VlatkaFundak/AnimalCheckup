ba3be749b14ed88f8311227b6e4ebfac13440630
VlatkaFundak <vlatka.fundak@gmail.com>
2017-06-17 14:06:32 +0200

    DI implementation

---
+++ src/AnimalCheckup/AnimalCheckup.Model/DIModule.cs
@@ -0,0 +1,22 @@
+﻿using AnimalCheckup.Model.Common.Models;
+using AnimalCheckup.Model.Models;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCheckup.Model
+{
+    public class DIModule : NinjectModule
+    {
+        /// <summary>
+        /// Load dependency injection bindings.
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IAnimalPoco>().To<AnimalPoco>();
+        }
+    }
+}
---
+++ src/AnimalCheckup/AnimalCheckup.Repository/DIModule.cs
@@ -0,0 +1,20 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCheckup.Repository
+{
+    public class DIModule : NinjectModule
+    {
+        /// <summary>
+        /// Load dependency injection bindings.
+        /// </summary>
+        public override void Load()
+        {
+            
+        }
+    }
+}
